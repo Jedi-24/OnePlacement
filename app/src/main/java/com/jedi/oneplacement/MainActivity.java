678b8eff29e72629051064a9dcc44d1ac6c931ca
Jedi-24 <jayagarwal2003@gmail.com>
2023-01-14 17:08:59 +0530

    Repository setup done

---
+++ app/src/main/java/com/jedi/oneplacement/MainActivity.java
@@ -0,0 +1,14 @@
+package com.jedi.oneplacement; // I am going to give it my ALL;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
