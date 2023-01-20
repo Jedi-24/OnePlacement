6bfa66b32fc00ae61c754ae4f71ea09168ca7ad2
Jedi-24 <jayagarwal2003@gmail.com>
2023-01-20 20:57:39 +0530

    server files

---
+++ onePlacement-server/src/test/java/com/Jedi/OnePlacementServer/OnePlacementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jedi.OnePlacementServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePlacementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
