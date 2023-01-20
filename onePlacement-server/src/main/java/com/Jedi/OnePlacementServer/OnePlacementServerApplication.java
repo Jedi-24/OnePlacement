22794fa5e58e8460a5453f3bf01df2b493f1d9d3
Jedi-24 <jayagarwal2003@gmail.com>
2023-01-20 20:19:24 +0530

    server files

---
+++ onePlacement-server/src/main/java/com/Jedi/OnePlacementServer/OnePlacementServerApplication.java
@@ -0,0 +1,14 @@
+package com.Jedi.OnePlacementServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePlacementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnePlacementServerApplication.class, args);
+
+
+	}
+}
